b8fc5446fb7af2b9849ed7fea5902ef4203da25f
Pascal Lüscher <luescherpascal@gmail.com>
2018-03-17 00:17:46 +0100

    updated bootstrap and other dependencies

---
+++ IP5-IRuettae/IRuettae.WebApp/Controllers/VisitController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IRuettae.Persistence.Entities;
+
+namespace IRuettae.WebApp.Controllers
+{
+    public class VisitController : Controller
+    {
+        // GET: Visit
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string AddVisit(Visit v)
+        {
+            return "hallo vater";
+        }
+    }
+}
