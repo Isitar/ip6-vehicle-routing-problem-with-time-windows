0a27708d812d1364624ba2ee25e3542d97ab44e0
Pascal Lüscher <luescherpascal@gmail.com>
2018-03-25 14:53:52 +0200

    added new admin controller

---
+++ IP5-IRuettae/IRuettae.WebApi/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IRuettae.WebApi.Persistence;
+
+namespace IRuettae.WebApi.Controllers
+{
+    public class AdminController : ApiController
+    {
+        public bool CleanDatabase()
+        {
+            
+        }
+    }
+}
