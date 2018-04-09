2c738e1f1a9e3ca6e9a0456a0574f45589d03335
Pascal Lüscher <pascal.luescher@asinfotrack.ch>
2018-04-09 17:04:57 +0200

    added santa table & mapping

---
+++ IP5-IRuettae/IRuettae.Persistence/Mappings/SantaMap.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Mapping;
+using IRuettae.Persistence.Entities;
+
+namespace IRuettae.Persistence.Mappings
+{
+    public class SantaMap : ClassMap<Santa>
+    {
+        public SantaMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+
+            HasMany(x => x.Breaks).KeyColumn("santa_id").Not.LazyLoad().Cascade.AllDeleteOrphan();
+        }
+    }
+}
