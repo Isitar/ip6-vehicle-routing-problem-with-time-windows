eb257774328613873c9e51e1e2187a6ea3cedd97
Janik Meyer <janik.meyer@students.fhnw.ch>
2018-04-22 14:33:26 +0200

    added more route tests

--- IP5-IRuettae/IRuettae.Core.Tests/Algorithm/RouteTest.cs
+++ IP5-IRuettae/IRuettae.Core.Tests/Algorithm/RouteTest.cs
@@ -39,5 +39,43 @@ namespace IRuettae.Core.Tests.Algorithm
 
             Assert.IsTrue(r1.Equals(r2));
         }
+
+        [TestMethod]
+        public void TestEquals_NotEqual()
+        {
+            var r1 = new Route(2, 2);
+            {
+                r1.Waypoints[0, 0].Add(1);
+                r1.Waypoints[0, 0].Add(2);
+                r1.Waypoints[0, 1].Add(3);
+                r1.Waypoints[0, 1].Add(4);
+                r1.Waypoints[1, 0].Add(5);
+                r1.Waypoints[1, 0].Add(6);
+                r1.Waypoints[1, 1].Add(7);
+                r1.Waypoints[1, 1].Add(8);
+            }
+            var r2 = new Route(2, 2);
+            {
+                r2.Waypoints[0, 0].Add(1);
+                r2.Waypoints[0, 0].Add(2);
+                r2.Waypoints[0, 1].Add(3);
+                r2.Waypoints[0, 1].Add(4);
+                r2.Waypoints[1, 0].Add(5);
+                r2.Waypoints[1, 0].Add(6);
+                r2.Waypoints[1, 1].Add(7);
+                r2.Waypoints[1, 1].Add(100000);
+            }
+
+            Assert.IsFalse(r1.Equals(r2));
+        }
+
+        [TestMethod]
+        public void TestEquals_Empty()
+        {
+            var r1 = new Route(2, 2);
+            var r2 = new Route(2, 2);
+
+            Assert.IsTrue(r1.Equals(r2));
+        }
     }
 }
