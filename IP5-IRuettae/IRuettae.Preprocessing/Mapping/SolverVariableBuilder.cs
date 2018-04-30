7ac189651963f56674a926e8b645a17011c07c8e
Pascal Lüscher <pascal.luescher@asinfotrack.ch>
2018-04-30 09:01:05 +0200

    draft state

---
+++ IP5-IRuettae/IRuettae.Preprocessing/Mapping/SolverVariableBuilder.cs
@@ -0,0 +1,50 @@
+﻿using IRuettae.Core.Algorithm;
+using IRuettae.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRuettae.Preprocessing.Mapping
+{
+    public class SolverVariableBuilder
+    {
+        private List<Santa> santas = null;
+        private List<Visit> visits = null;
+        private List<Way> ways = null;
+
+        /// <summary>
+        /// List of days(StartTime, EndTime)
+        /// </summary>
+        private List<(DateTime, DateTime)> days = null;
+
+        public SolverVariableBuilder(List<Santa> santas = null, List<Visit> visits = null, List<Way> ways = null, List<(DateTime, DateTime)> days = null)
+        {
+            this.santas = santas;
+            this.visits = visits;
+            this.ways = ways;
+            this.days = days;
+        }
+
+
+
+        public SolverInputData Build()
+        {
+            //bool[][,] santas
+
+            foreach (var day in days)
+            {
+
+            }
+
+            
+            //     VisitState[][,] visits = {
+            //     int[,] distances = {
+
+            //         int[] visitLength =
+            //{
+            return null;
+        }
+    }
+}
