20f015506c7ebbf18a18d117905a17d88884e9e7
Pascal Lüscher <luescherpascal@gmail.com>
2018-05-01 19:22:02 +0200

    added frontend

--- IP5-IRuettae/IRuettae.WebApi/Controllers/AlgorithmController.cs
+++ IP5-IRuettae/IRuettae.WebApi/Controllers/AlgorithmController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using IRuettae.Core.Algorithm;
 using IRuettae.Persistence.Entities;
 using IRuettae.Preprocessing.Mapping;
+using IRuettae.WebApi.Helpers;
 using IRuettae.WebApi.Models;
 using IRuettae.WebApi.Persistence;
 
@@ -15,25 +16,30 @@ namespace IRuettae.WebApi.Controllers
     public class AlgorithmController : ApiController
     {
         [HttpPost]
-        public Route CalculateRoute(int year, int timeslotLength = 5 * 60, int timePerChild = 15, int beta0Time = 5)
+        public Route CalculateRoute([FromBody] AlgorithmStarter algorithmStarter)
         {
 
             using (var dbSession = SessionFactory.Instance.OpenSession())
             using (var transaction = dbSession.BeginTransaction())
             {
                 var visits = dbSession.Query<Visit>().Take(10).ToList();
-                visits.ForEach(v => v.Duration = 60* (v.NumberOfChildren * timePerChild + beta0Time));
+                visits.ForEach(v => v.Duration = 60 * (v.NumberOfChildren * algorithmStarter.TimePerChild + algorithmStarter.Beta0));
+                visits.Sort((a, b) =>
+                {
+                    if (a.Id == algorithmStarter.StarterId)
+                    {
+                        return -1;
+                    }
+                    return a.Id.CompareTo(b.Id);
+                });
 
-                var solverVariableBuilder = new SolverVariableBuilder(timeslotLength)
+                var solverVariableBuilder = new SolverVariableBuilder(algorithmStarter.TimeSliceDuration)
                 {
                     Visits = visits,
                     Santas = dbSession.Query<Santa>().ToList(),
-                    Days = new List<(DateTime, DateTime)>
-                    {
-                        (new DateTime(2017, 12, 6, 17, 00, 00), new DateTime(2017, 12, 6, 22, 00, 00)),
-                        (new DateTime(2017, 12, 7, 17, 00, 00), new DateTime(2017, 12, 7, 22, 00, 00))
-                    }
+                    Days = algorithmStarter.Days
                 };
+                
                 var solverInputData = solverVariableBuilder.Build();
                 return Starter.Optimise(solverInputData);
             }
