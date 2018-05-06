b388ffab203cde90568449ae9891563bf0cc5206
Janik Meyer <janik.meyer@students.fhnw.ch>
2018-05-06 14:30:27 +0200

    added test for multiple santas

---
+++ IP5-IRuettae/IRuettae.Core.Tests/Algorithm/MultipleSantaTest.cs
@@ -0,0 +1,63 @@
+﻿using IRuettae.Core.Algorithm;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IRuettae.Core.Tests.Algorithm
+{
+    [TestClass]
+    public class MultipleSantaTest
+    {
+        public SolverInputData GetModel()
+        {
+            const bool t = true;
+            //const bool f = false;
+            bool[][,] santas =
+            {
+                new[,]
+                {
+                    {t, t, t, t, t, t, t, t},
+                    {t, t, t, t, t, t, t, t},
+                }
+            };
+
+            VisitState d = VisitState.Default;
+            VisitState[][,] visits =
+            {
+                new[,]
+                {
+                    {d, d, d, d, d, d, d, d},
+                    {d, d, d, d, d, d, d, d},
+                    {d, d, d, d, d, d, d, d},
+                },
+
+            };
+
+            //var X = int.MaxValue;
+            int[,] distances =
+            {
+                {0, 1, 1},
+                {1, 0, 10},
+                {1, 10, 0},
+            };
+
+            int[] visitLength =
+            {
+                0, 1, 1,
+            };
+
+            return new SolverInputData(santas, visitLength, visits, distances);
+        }
+
+
+        [TestMethod]
+        public void TestMinTimeOnly()
+        {
+            var model = GetModel();
+            var result = Starter.Optimise(model, TargetBuilderType.MinTimeOnly);
+
+            Assert.AreEqual(2, result.Waypoints.GetLength(0));
+            Assert.AreEqual(3, result.Waypoints[0, 0].Count);
+            Assert.AreEqual(3, result.Waypoints[1, 0].Count);
+        }
+    }
+}
