bfbd1c0b20e4c651ef64d5ebe347871c025d3dd5
Janik Meyer <janik.meyer@students.fhnw.ch>
2018-05-07 16:34:19 +0200

    reset ConsoleApp.Test to 5

--- IP5-IRuettae/IRuettae.ConsoleApp/Test.cs
+++ IP5-IRuettae/IRuettae.ConsoleApp/Test.cs
@@ -18,12 +18,12 @@ namespace IRuettae.ConsoleApp
     {
         internal static void Run(string[] args)
         {
-        //    ExportMPSVisits(5);
-        //    ExportMPSVisits(10);
-        //    ExportMPSVisits(15);
-        //    ExportMPSVisits(20);
-        //    ExportMPSVisits(29);
-            TestAlgorithm(12);
+            //ExportMPSVisits(5);
+            //ExportMPSVisits(10);
+            //ExportMPSVisits(15);
+            //ExportMPSVisits(20);
+            //ExportMPSVisits(29);
+            TestAlgorithm(5);
         }
 
 
@@ -34,7 +34,7 @@ namespace IRuettae.ConsoleApp
             {
                 TestSerailDataVisits($"SerializedObjects/SolverInput{n_visits}Visits.serial");
             }
-            
+
         }
 
         private static void ExportMPSVisits(int n_visits)
