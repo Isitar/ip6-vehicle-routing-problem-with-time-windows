eb2f5b98f2af65a6549a73fe31b8fa8fb0c556a5
Janik Meyer <janik.meyer@students.fhnw.ch>
2018-06-04 16:52:04 +0200

    little UI improvement

--- IP5-IRuettae/IRuettae.WebApp/Models/AlgorithmStarterVM.cs
+++ IP5-IRuettae/IRuettae.WebApp/Models/AlgorithmStarterVM.cs
@@ -29,10 +29,10 @@ namespace IRuettae.WebApp.Models
         [DisplayName("Timeslice Dauer [s]")]
         public int TimeSliceDuration { get; set; }
         [Required]
-        [DisplayName("Zeit pro Kind [min]")]
+        [DisplayName("Zeit pro weiteres Kind [min]")]
         [Range(1, int.MaxValue)]
         public int TimePerChild { get; set; }
-        [Range(0, int.MaxValue)]
+        [Range(1, int.MaxValue)]
         [DisplayName("Zeit für erstes Kind [min]")]
         public int Beta0 { get; set; }
 
