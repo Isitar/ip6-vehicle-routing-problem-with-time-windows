74218f02f845d7d4f08355d6238c9189d37f4c77
Pascal Lüscher <luescherpascal@gmail.com>
2018-10-05 21:58:14 +0200

    added index operator to way matrix, added code to convert function to handle visits and santas

--- IRuettae/Converter/IRuettae.Converter/PersistenceToCoreConverter.cs
+++ IRuettae/Converter/IRuettae.Converter/PersistenceToCoreConverter.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using IRuettae.Core.Models;
-using Visit = IRuettae.Persistence.Entities.Visit;
+
+
 
 namespace IRuettae.Converter
 {
@@ -13,13 +13,69 @@ namespace IRuettae.Converter
         /// <summary>
         /// Converts the input params to an OptimisationInput
         /// </summary>
+        /// <param name="workingDays">All working days for the santas</param>
+        /// <param name="startVisit">Where all routes have to start</param>
         /// <param name="visits">All visits for the problem</param>
         /// <param name="santas">All santas for the problem</param>
-        /// <param name="breaks">All breaks for the problem</param>
+        /// <param name="breaks">All breaks for the santas</param>
         /// <returns>An optimisation input that can be used to solve the problem</returns>
-        public OptimisationInput Convert(List<Visit> visits, List<Santa> santas, List<Visit> breaks)
+        public Core.Models.OptimisationInput Convert(List<(DateTime, DateTime)> workingDays, Persistence.Entities.Visit startVisit, List<Persistence.Entities.Visit> visits, List<Persistence.Entities.Santa> santas, List<Persistence.Entities.Visit> breaks)
         {
-            throw new NotImplementedException();
+            var input = new Core.Models.OptimisationInput
+            {
+                WayMatrix = new Core.Models.WayMatrix
+                {
+                    RouteCosts = new int[visits.Count, visits.Count]
+                },
+                Visits = new Core.Models.Visit[visits.Count],
+                Santas = new Core.Models.Santa[santas.Count * workingDays.Count],
+            };
+
+            // set 0-time
+            var zeroTime = workingDays.Min(wd => wd.Item1);
+
+            // create visits
+
+            // sort visits to be sure they are in id order
+            visits = visits.OrderBy(v => v.Id).ToList();
+
+            for (int x = 0; x < visits.Count; x++)
+            {
+                var persistenceVisit = visits[x];
+                input.Visits[x] = new Core.Models.Visit()
+                {
+                    Id = x,
+                    Desired = persistenceVisit.Desired
+                        .Select(d => ((d.Start.Value - zeroTime).Seconds, (d.End.Value - zeroTime).Seconds)).ToArray(),
+                    Unavailable = persistenceVisit.Unavailable
+                        .Select(d => ((d.Start.Value - zeroTime).Seconds, (d.End.Value - zeroTime).Seconds)).ToArray(),
+                    Duration = persistenceVisit.NumberOfChildren * 5 + 15,
+                    WayCostFromHome = startVisit.ToWays.First(w => w.To.Id.Equals(persistenceVisit.Id)).Duration,
+                    WayCostToHome = startVisit.FromWays.First(w => w.To.Id.Equals(persistenceVisit.Id)).Duration,
+                };
+
+                // fill distance matrix
+                for (int y = 0; y < visits.Count; y++)
+                {
+                    if (x == y)
+                    {
+                        input.WayMatrix[x, y] = 0;
+                    }
+                    else
+                    {
+                        input.WayMatrix[x, y] = persistenceVisit.FromWays.First(w => w.To.Id.Equals(visits[y].Id)).Duration;
+                    }
+                }
+            }
+
+            for (int i = 0; i < santas.Count * workingDays.Count; i++)
+            {
+                input.Santas[i] = new Core.Models.Santa { Id = i };
+            }
+
+            // todo: work from here ?break handling?
+
+            return input;
         }
     }
 }
--- IRuettae/Core/IRuettae.Core/Models/WayMatrix.cs
+++ IRuettae/Core/IRuettae.Core/Models/WayMatrix.cs
@@ -7,14 +7,28 @@ using System.Threading.Tasks;
 namespace IRuettae.Core.Models
 {
     /// <summary>
-    /// Wrapper struct containing the route cost for all possible ways
+    /// Wrapper class containing the route cost for all possible ways
     /// </summary>
-    public struct WayMatrix
+    public class WayMatrix
     {
         /// <summary>
         /// Matrix containing the route costs
         /// x -> y is on position [x,y], y->x on [y,x]
         /// </summary>
         public int[,] RouteCosts;
+
+
+        /// <summary>
+        /// Route costs from x to y
+        /// </summary>
+        /// <param name="x">From this point</param>
+        /// <param name="y">To this point</param>
+        /// <returns>The route cost</returns>
+        public int this[int x, int y]
+        {
+            get => RouteCosts[x, y];
+            set => RouteCosts[x, y] = value;
+        }
+
     }
 }
