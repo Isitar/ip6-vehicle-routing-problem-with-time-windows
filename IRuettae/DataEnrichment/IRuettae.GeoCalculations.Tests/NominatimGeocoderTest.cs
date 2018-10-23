765d02264dc69419fe4970172d44d991ae130184
Pascal Lüscher <luescherpascal@gmail.com>
2018-10-23 23:16:47 +0200

    added nominatim geocoder (osm)

---
+++ IRuettae/DataEnrichment/IRuettae.GeoCalculations.Tests/NominatimGeocoderTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using IRuettae.GeoCalculations.Geocoding;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IRuettae.GeoCalculations.Tests
+{
+    [TestClass]
+    public class NominatimGeocoderTest
+    {
+        
+        private const string region = "CH";
+        private const string referer = "ip6.isitar.ch";
+        private const string email = "luescherpascal@gmail.com";
+
+        [TestMethod]
+        public void TestCityFromZip()
+        {
+            var geocoder = new NominatimGeocoder(email,region,referer);
+            Assert.IsNotNull(geocoder);
+
+            var cities = geocoder.CityFromZip(5600);
+            Assert.AreEqual(2, cities.Length);
+            Assert.IsNotNull(cities.Where(c => c.Equals("Lenzburg")));
+
+            var cities2 = geocoder.CityFromZip(4852);
+            Assert.AreEqual(1, cities2.Length);
+            Assert.AreEqual("Rothrist", cities2[0]);
+
+
+        }
+
+        [TestMethod]
+        public void TestCityFromZipSpecialChars()
+        {
+            var geocoder = new NominatimGeocoder(email, region, referer);
+            var cities3 = geocoder.CityFromZip(3178);
+            Assert.AreEqual("Bösingen", cities3[0]);
+        }
+
+        [TestMethod]
+        public void TestCoords()
+        {
+            var geocoder = new NominatimGeocoder(email, region, referer);
+            var coords = geocoder.Locate("Schafisheimerstrasse 12 5603 Staufen");
+            Assert.AreEqual(47.38376435,coords.lat);
+            Assert.AreEqual(8.1582365, coords.lng);
+        }
+    }
+}
---
+++ IRuettae/DataEnrichment/IRuettae.Geolocator/Geocoding/NominatimGeocoder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace IRuettae.GeoCalculations.Geocoding
+{
+    public class NominatimGeocoder : IGeocoder
+    {
+        private readonly string email;
+        private readonly string region;
+        private readonly string referer;
+
+        /// <summary>
+        /// Creates a geocoder instance with nominatim backend
+        /// </summary>
+        /// <param name="email">email used for nomatim request</param>
+        /// <param name="region">country code filter</param>
+        /// <param name="referer">site referer</param>
+        public NominatimGeocoder(string email, string region, string referer)
+        {
+            this.email = email;
+            this.region = region;
+            this.referer = referer;
+        }
+        public (double lat, double lng) Locate(string address)
+        {
+            var wc = new WebClient { Encoding = Encoding.UTF8 };
+            wc.Headers.Add("Referer", referer);
+            var uri = $"https://nominatim.openstreetmap.org/search?q={Uri.EscapeDataString(address)}&format=json&email={email}";
+            var retJson = wc.DownloadString(uri);
+            try
+            {
+                dynamic retData = JArray.Parse(retJson)[0];
+
+                double lat = retData.lat;
+                double lon = retData.lon;
+
+                return (lat, lon);
+
+            }
+            catch
+            {
+                throw new LocationNotFoundException();
+            }
+        }
+
+        public string[] CityFromZip(int zip)
+        {
+            var wc = new WebClient { Encoding = Encoding.UTF8 };
+            wc.Headers.Add("Referer", referer);
+            var uri = $"https://nominatim.openstreetmap.org/search?postalcode={zip}&countrycodes={region}&format=json&email={email}";
+            var retJson = wc.DownloadString(uri);
+            try
+            {
+                dynamic retData = JArray.Parse(retJson)[0];
+
+                double lat = retData.lat;
+                double lon = retData.lon;
+                retJson = wc.DownloadString($"https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json&email={email}");
+
+                retData = JObject.Parse(retJson);
+                return new string[] { retData.address.village.ToString() };
+            }
+            catch
+            {
+                throw new LocationNotFoundException();
+            }
+        }
+
+    }
+}
