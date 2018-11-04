7391d03ee48696e97a1147c3372c52b9e862ca40
Janik Meyer <janik.my@gmail.com>
2018-11-04 18:10:12 +0100

    added project for genetic algorithm

---
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Models/GenAlgStarterData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRuettae.Core.GeneticAlgorithm.Algorithm.Models
+{
+    public class GenAlgStarterData
+    {
+    }
+}
---
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/GenAlgSolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IRuettae.Core.GeneticAlgorithm.Algorithm.Models;
+using IRuettae.Core.Models;
+using Route = IRuettae.Core.Models.Route;
+using Waypoint = IRuettae.Core.Models.Waypoint;
+
+namespace IRuettae.Core.GeneticAlgorithm
+{
+    public class GenAlgSolver : ISolver
+    {
+        private readonly OptimizationInput input;
+        private readonly GenAlgStarterData starterData;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="starterData"></param>
+        public GenAlgSolver(OptimizationInput input, GenAlgStarterData starterData)
+        {
+            this.input = input;
+            this.starterData = starterData;
+        }
+
+        public OptimizationResult Solve(int timelimit, EventHandler<ProgressReport> progress, EventHandler<string> consoleProgress)
+        {
+            return null;
+        }
+    }
+}
