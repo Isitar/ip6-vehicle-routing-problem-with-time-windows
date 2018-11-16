fff9b05bc1cdfd63077ca1b0f14ce9eda4ad0ff8
Janik Meyer <janik.my@gmail.com>
2018-11-16 14:32:34 +0100

    added CostCalculator

---
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/CostCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IRuettae.Core.GeneticAlgorithm.Algorithm.Models;
+using IRuettae.Core.Models;
+
+namespace IRuettae.Core.GeneticAlgorithm.Algorithm
+{
+    /// <summary>
+    /// Class to recalculate the cost of Genotypes
+    /// </summary>
+    public class CostCalculator
+    {
+        private OptimizationResult result;
+        private Decoder decoder;
+
+        public CostCalculator(Decoder decoder, OptimizationResult temporaryResult)
+        {
+            this.decoder = decoder;
+            this.result = temporaryResult;
+        }
+
+        private void RecalculatateCost(List<Genotype> population)
+        {
+            foreach (var individual in population)
+            {
+                result.Routes = decoder.Decode(individual);
+                individual.Cost = result.Cost();
+            }
+        }
+
+        public void RecalculatateCost(Genotype individual)
+        {
+            result.Routes = decoder.Decode(individual);
+            individual.Cost = result.Cost();
+        }
+    }
+}
