7c24bedbe4b184858025867e7cf2f15b4819896c
Janik Meyer <janik.my@gmail.com>
2018-11-22 12:13:37 +0100

    added draft of TournamentSelection, WIP #19

--- IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Helpers/ExtensionMethods.cs
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Helpers/ExtensionMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,6 +30,27 @@ namespace IRuettae.Core.GeneticAlgorithm.Algorithm.Helpers
                 list[k] = list[n];
                 list[n] = value;
             }
+        }
+
+        /// <summary>
+        /// Returns a random number between min and max.
+        /// Note: This function is focussed on performance
+        /// and therefore it may have an imbalance in the probability of distrubution.
+        /// Precondition: min < max
+        /// </summary>
+        /// <param name="random">not null</param>
+        /// <param name="min">lowest possible return value</param>
+        /// <param name="max">highest possible return value</param>
+        /// <returns></returns>
+        public static int NextInt(this RandomNumberGenerator random, int min, int max)
+        {
+            var data = new byte[4];
+            random.GetBytes(data);
+
+            // scale
+            var generatedValue = Math.Abs(BitConverter.ToInt32(data, 0));
+            var mod = generatedValue % (max - min);
+            return min + mod;
         }
     }
 }
---
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/TournamentSelection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using IRuettae.Core.GeneticAlgorithm.Algorithm.Helpers;
+using IRuettae.Core.GeneticAlgorithm.Algorithm.Models;
+
+namespace IRuettae.Core.GeneticAlgorithm.Algorithm
+{
+    /// <summary>
+    /// Deterministic binary tournament selection.
+    /// The two individuals for the tournament are selected randomly.
+    /// </summary>
+    public class BinaryTournamentSelection
+    {
+        private readonly RandomNumberGenerator rng;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="rng">not null</param>
+        public BinaryTournamentSelection(RandomNumberGenerator rng)
+        {
+            this.rng = rng;
+        }
+
+        /// <summary>
+        /// Returns numberOfPairs pairs of genotypes from population which should reproduce.
+        /// </summary>
+        /// <param name="population">with calculated costs and count > 0</param>
+        /// <param name="numberOfPairs"></param>
+        /// <returns></returns>
+        public List<(Genotype, Genotype)> Select(List<Genotype> population, int numberOfPairs)
+        {
+            var ret = new List<(Genotype, Genotype)>(numberOfPairs);
+            while (numberOfPairs-- > 0)
+            {
+                var parent1 = GetParent(population);
+                var parent2 = GetParent(population);
+                ret.Add((parent1, parent2));
+            }
+            return ret;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="population">count > 0</param>
+        /// <returns></returns>
+        private Genotype GetParent(List<Genotype> population)
+        {
+            var candidate1 = GetRandomIndividual(population);
+            var candidate2 = GetRandomIndividual(population);
+            return candidate1.Cost < candidate2.Cost ? candidate1 : candidate2;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="population">count > 0</param>
+        /// <returns></returns>
+        private Genotype GetRandomIndividual(List<Genotype> population)
+        {
+            var index = rng.NextInt(0, population.Count - 1);
+            return population[index];
+        }
+    }
+}
