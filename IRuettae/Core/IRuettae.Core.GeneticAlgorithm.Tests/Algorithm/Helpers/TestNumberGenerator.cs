fcdba07a65d601dfc33cf90f5e149a59daecb704
Janik Meyer <janik.my@gmail.com>
2018-11-22 12:30:04 +0100

    added TestNumberGenerator

---
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm.Tests/Algorithm/Helpers/TestNumberGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRuettae.Core.GeneticAlgorithm.Tests.Algorithm.Helpers
+{
+    public class TestNumberGenerator : RandomNumberGenerator
+    {
+        /// <summary>
+        /// Will increase with each call to GetBytes
+        /// </summary>
+        int counter = 0;
+
+        public override void GetBytes(byte[] data)
+        {
+            byte[] randomBytes = BitConverter.GetBytes(counter++);
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = randomBytes[i % randomBytes.Length];
+            }
+        }
+    }
+}
---
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm.Tests/Algorithm/Helpers/TestNumberGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IRuettae.Core.GeneticAlgorithm.Tests.Algorithm.Helpers
+{
+    [TestClass]
+    public class TestNumberGeneratorTests
+    {
+        [TestMethod]
+        public void TestGetBytes()
+        {
+            var generator = new TestNumberGenerator();
+            var bytes = new byte[4];
+
+            for (int expected = 0; expected < 1000; expected++)
+            {
+                generator.GetBytes(bytes);
+                Assert.AreEqual(expected, BitConverter.ToInt32(bytes, 0));
+            }
+        }
+    }
+}
