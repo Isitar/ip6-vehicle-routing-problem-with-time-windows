e1d4cae0f8087c98c7b992dc68e34f1fddbf43f3
Isitar <luescherpascal@gmail.com>
2018-11-26 11:34:58 +0100

    added unit test, testing for unavailability

--- IRuettae/Core/IRuettae.Core.LocalSolverTests/SolverTests.cs
+++ IRuettae/Core/IRuettae.Core.LocalSolverTests/SolverTests.cs
@@ -111,7 +111,59 @@ namespace IRuettae.Core.LocalSolver.Tests
             Assert.AreEqual(1010, output.Cost());
             Assert.AreEqual(1, output.Routes[0].Waypoints.Skip(1).First().VisitId);
             Assert.AreEqual(0, output.Routes[0].Waypoints.First().StartTime);
-            Assert.AreEqual(2*hour, output.Routes[0].Waypoints[1].StartTime);
+            Assert.AreEqual(2 * hour, output.Routes[0].Waypoints[1].StartTime);
+        }
+
+        [TestMethod]
+        public void TestRouteUnavailableCorrect()
+        {
+            //   B -1- A
+            //   2|  /3
+            //    |/
+            //    H
+
+            const int hour = 3600;
+            var input = new OptimizationInput
+            {
+                Visits = new[]
+                {
+                    new Visit
+                    {
+                        Id = 0, //A
+                        Duration = 4 * hour,
+                        Desired = new (int from, int to)[0],
+                        Unavailable = new (int from, int to)[] {(7*hour, 100*hour)},
+                        WayCostToHome = 3*hour,
+                        WayCostFromHome = 3*hour
+                    },
+                    new Visit
+                    {
+                        Id = 1, // B
+                        Duration = 5*hour,
+                        Desired = new (int from, int to)[0],
+                        Unavailable = new (int from, int to)[0],
+                        WayCostToHome = 2*hour,
+                        WayCostFromHome = 2*hour,
+                    },
+                },
+                Santas = new[] { new Santa { Id = 0 }, },
+                Days = new (int from, int to)[] { (0, 100 * hour) },
+                RouteCosts = new[,]
+                {
+                    {0, hour},
+                    {hour, 0},
+                },
+            };
+
+            var solver = new Solver(input);
+            var output = solver.Solve(3000L, null, null);
+            Assert.IsNotNull(output);
+            Assert.IsNotNull(output.Routes);
+            Assert.AreEqual(1, output.NonEmptyRoutes.Count());
+            Assert.AreEqual(1050, output.Cost());
+            Assert.AreEqual(0, output.Routes[0].Waypoints.Skip(1).First().VisitId);
+            Assert.AreEqual(0, output.Routes[0].Waypoints.First().StartTime);
+            Assert.AreEqual(3 * hour, output.Routes[0].Waypoints[1].StartTime);
         }
     }
 }
