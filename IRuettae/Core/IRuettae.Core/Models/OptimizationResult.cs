fc582e92e46ec0948f3bb7dcd04b3473fb15e285
Janik Meyer <janik.my@gmail.com>
2018-11-28 15:35:50 +0100

    put virtual in the right place

--- IRuettae/Core/IRuettae.Core/Models/OptimizationResult.cs
+++ IRuettae/Core/IRuettae.Core/Models/OptimizationResult.cs
@@ -71,13 +71,13 @@ namespace IRuettae.Core.Models
             return (int)Math.Ceiling(cost);
 
         }
-        public int virtual NumberOfNotVisitedFamilies()
+        public virtual int NumberOfNotVisitedFamilies()
         {
             var visitedVisits = NonEmptyRoutes.SelectMany(r => r.Waypoints.Select(w => w.VisitId));
             return OptimizationInput.Visits.Count(v => !v.IsBreak && !visitedVisits.Contains(v.Id));
         }
 
-        public int virtual NumberOfMissingBreaks()
+        public virtual int NumberOfMissingBreaks()
         {
             var santaBreaks = new Dictionary<int, int>();
             foreach (var v in OptimizationInput.Visits.Where(v => v.IsBreak))
