a231bda6c9cbb35b77baad3b5704dd0bb678ecb4
Janik Meyer <janik.my@gmail.com>
2018-12-10 15:06:29 +0100

    reset PopulationSize of GA to 10

--- IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Models/GenAlgStarterData.cs
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Models/GenAlgStarterData.cs
@@ -14,6 +14,10 @@ namespace IRuettae.Core.GeneticAlgorithm.Algorithm.Models
         public long MaxNumberOfGenerations { get; set; } = long.MaxValue;
         public double MutationProbability { get; set; } = 0.1;
 
+        // Todo: remove
+        static int callCounter = 0;
+        const int runs = 3;
+
         /// <summary>
         ///Create default regarding the input
         /// </summary>
@@ -21,8 +25,15 @@ namespace IRuettae.Core.GeneticAlgorithm.Algorithm.Models
         /// <returns></returns>
         public static GenAlgStarterData GetDefault(OptimizationInput input)
         {
+            var populationSizes = new int[]
+            {
+                5,
+                25,
+            };
+
             return new GenAlgStarterData()
             {
+                //PopulationSize = populationSizes[(callCounter++ / runs) % populationSizes.Length],
                 MaxNumberOfSantas = input.Santas.Length,
             };
         }
