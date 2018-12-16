4c201c030d94aacbb61388bf8c783852d777ffc2
Pascal Lüscher <luescherpascal@gmail.com>
2018-12-16 14:38:47 +0100

    added breaks to dataset generator

--- IRuettae/DebugApps/IRuettae.DatasetGenerator/Program.cs
+++ IRuettae/DebugApps/IRuettae.DatasetGenerator/Program.cs
@@ -7,16 +7,16 @@ namespace IRuettae.DatasetGenerator
     {
         static void GenerateAllDatasets()
         {
-            File.WriteAllText("DatasetFactory.DataSet1.cs", new DatasetGenerator(4000, 4000, 10, 2, 1, new[] { 0, 0 }, new[] { 0, 0 }, -1).Generate("DataSet1"));
-            File.WriteAllText("DatasetFactory.DataSet2.cs", new DatasetGenerator(4000, 4000, 10, 2, 1, new[] { 5, 5 }, new[] { 0, 0 }, -1).Generate("DataSet2"));
-            File.WriteAllText("DatasetFactory.DataSet3.cs", new DatasetGenerator(4000, 4000, 10, 2, 1, new[] { 0, 0 }, new[] { 5, 5 }, -1).Generate("DataSet3"));
-            File.WriteAllText("DatasetFactory.DataSet4.cs", new DatasetGenerator(4000, 4000, 20, 2, 2, new[] { 0, 0 }, new[] { 0, 0 }, -1).Generate("DataSet4"));
-            File.WriteAllText("DatasetFactory.DataSet5.cs", new DatasetGenerator(4000, 4000, 20, 2, 2, new[] { 10, 10 }, new[] { 0, 0 }, -1).Generate("DataSet5"));
-            File.WriteAllText("DatasetFactory.DataSet6.cs", new DatasetGenerator(4000, 4000, 20, 2, 2, new[] { 0, 0 }, new[] { 10, 10 }, -1).Generate("DataSet6"));
-            File.WriteAllText("DatasetFactory.DataSet9.cs", new DatasetGenerator(4000, 4000, 50, 2, 5, new[] { 15, 15 }, new[] { 11, 11 }, -1).Generate("DataSet9"));
-            File.WriteAllText("DatasetFactory.DataSet10.cs", new DatasetGenerator(4000, 4000, 100, 2, 10, new[] { 35, 35 }, new[] { 20, 20 }, -1).Generate("DataSet10"));
-            File.WriteAllText("DatasetFactory.DataSet11.cs", new DatasetGenerator(4000, 4000, 200, 2, 20, new[] { 75, 75 }, new[] { 40, 40 }, -1).Generate("DataSet11"));
-            File.WriteAllText("DatasetFactory.DataSet12.cs", new DatasetGenerator(4000, 4000, 1000, 2, 100, new[] { 300, 300 }, new[] { 150, 150 }, -1).Generate("DataSet12"));
+            File.WriteAllText("DatasetFactory.DataSet1.cs", new DatasetGenerator(4000, 4000, 10, 2, 1, new[] { 0, 0 }, new[] { 0, 0 }, -1, false).Generate("DataSet1"));
+            File.WriteAllText("DatasetFactory.DataSet2.cs", new DatasetGenerator(4000, 4000, 10, 2, 1, new[] { 5, 5 }, new[] { 0, 0 }, -1, false).Generate("DataSet2"));
+            File.WriteAllText("DatasetFactory.DataSet3.cs", new DatasetGenerator(4000, 4000, 10, 2, 1, new[] { 0, 0 }, new[] { 5, 5 }, -1, false).Generate("DataSet3"));
+            File.WriteAllText("DatasetFactory.DataSet4.cs", new DatasetGenerator(4000, 4000, 20, 2, 2, new[] { 0, 0 }, new[] { 0, 0 }, -1, false).Generate("DataSet4"));
+            File.WriteAllText("DatasetFactory.DataSet5.cs", new DatasetGenerator(4000, 4000, 20, 2, 2, new[] { 10, 10 }, new[] { 0, 0 }, -1, true).Generate("DataSet5"));
+            File.WriteAllText("DatasetFactory.DataSet6.cs", new DatasetGenerator(4000, 4000, 20, 2, 2, new[] { 0, 0 }, new[] { 10, 10 }, -1, false).Generate("DataSet6"));
+            File.WriteAllText("DatasetFactory.DataSet9.cs", new DatasetGenerator(4000, 4000, 50, 2, 5, new[] { 15, 15 }, new[] { 11, 11 }, -1, true).Generate("DataSet9"));
+            File.WriteAllText("DatasetFactory.DataSet10.cs", new DatasetGenerator(4000, 4000, 100, 2, 10, new[] { 35, 35 }, new[] { 20, 20 }, -1, true).Generate("DataSet10"));
+            File.WriteAllText("DatasetFactory.DataSet11.cs", new DatasetGenerator(4000, 4000, 200, 2, 20, new[] { 75, 75 }, new[] { 40, 40 }, -1, true).Generate("DataSet11"));
+            File.WriteAllText("DatasetFactory.DataSet12.cs", new DatasetGenerator(4000, 4000, 1000, 2, 100, new[] { 300, 300 }, new[] { 150, 150 }, -1, true).Generate("DataSet12"));
 
         }
 
