85f243eb2d5216d03937f255085c5c14d43f4b57
Janik Meyer <janik.my@gmail.com>
2018-12-24 12:37:36 +0100

    fixed error in PSO, renamed some variables

--- IRuettae/DebugApps/IRuettae.GeneticAlgorithmTuning/ParticleSwarmOptimization.cs
+++ IRuettae/DebugApps/IRuettae.GeneticAlgorithmTuning/ParticleSwarmOptimization.cs
@@ -8,13 +8,13 @@ namespace IRuettae.GeneticAlgorithmTuning
 {
     internal class ParticleSwarmOptimization
     {
-        private readonly int n;
-        const int ngen = 150;
-        const int npop = 100;
+        private readonly int numberOfVars;
+        const int NumberOfGenerations = 150;
+        const int PopulationSize = 100;
         const double xmin = -5;
         const double xmax = 5;
 
-        private double w = 1; // insertia weight
+        private double w = 1; // inertia weight
         private double c1 = 2; // cognitive parameter
         private double c2 = 2; // social parameter
 
@@ -23,73 +23,77 @@ namespace IRuettae.GeneticAlgorithmTuning
         public ParticleSwarmOptimization(Func<double[], double> objectiveFunction, int numberOfDesignVariables)
         {
             objective = objectiveFunction;
-            n = numberOfDesignVariables;
+            numberOfVars = numberOfDesignVariables;
         }
 
         public void Run()
         {
             var random = new Random();
-            var x = new double[npop][]; // design variable vector's
-            var v = new double[npop, n]; // design velocity vector's
-            var f = new double[npop]; // best "fitness"
-            var ibest = new double[npop, n]; // individual best position
-            var gbest = new double[n]; // global best position
-            var fbest = double.MaxValue; // global best fitness
-            double fnew; // global best fitness
+            // design variable vector's
+            var x = new double[PopulationSize][];
+            var bestFitness = new double[PopulationSize]; // best "fitness"
 
             // init
-            for (int p = 0; p < npop; p++)
+            for (int p = 0; p < PopulationSize; p++)
             {
-                for (int m = 0; m < n; m++)
+                for (int m = 0; m < numberOfVars; m++)
                 {
-                    x[p] = new double[n];
+                    x[p] = new double[numberOfVars];
                     x[p][m] = xmin + (xmax - xmin) * random.NextDouble();
                 }
-                f[p] = double.MaxValue;
+                bestFitness[p] = double.MaxValue;
             }
 
+            var invididualBestPosition = new double[PopulationSize, numberOfVars]; // individual best position
+            var velocity = new double[PopulationSize, numberOfVars]; // design velocity vector's
+            var globalBestFitness = double.MaxValue; // global best fitness
+            var globalBestPosition = new double[numberOfVars]; // global best position
+
             // hier startet die "Evolution"
-            for (int g = 1; g < ngen; g++)
+            for (var g = 1; g <= NumberOfGenerations; g++)
             {
                 // Loop über Generationen
 
-                for (int p = 0; p < npop; p++)
+                for (int p = 0; p < PopulationSize; p++)
                 {
                     // Suche individual best und global best
-                    fnew = objective(x[p]);
-                    if (fnew < f[p])
+                    var newFitness = objective(x[p]);
+                    if (newFitness < bestFitness[p])
                     {
                         // Individual best
-                        f[p] = fnew;
-                        for (int m = 0; m < n; m++)
+                        bestFitness[p] = newFitness;
+                        for (int m = 0; m < numberOfVars; m++)
                         {
-                            ibest[p, m] = x[p][m];
+                            invididualBestPosition[p, m] = x[p][m];
                         }
-                        fbest = fnew;
-                        // Global best
-                        for (int m = 0; m < n; m++)
+                        if (newFitness < globalBestFitness)
                         {
-                            gbest[m] = x[p][m];
+                            // global best
+                            globalBestFitness = newFitness;
+                            for (int m = 0; m < numberOfVars; m++)
+                            {
+                                globalBestPosition[m] = x[p][m];
+                            }
                         }
                     }
                 }
 
                 // Output progress
                 Console.WriteLine($"generation {g}");
-                for (int m = 0; m < n; m++)
+                for (int m = 0; m < numberOfVars; m++)
                 {
-                    Console.WriteLine($"global best[{m}]={gbest[m]}");
+                    Console.WriteLine($"global best[{m}]={globalBestPosition[m]}");
                 }
-                Console.WriteLine($"w={w} f={fbest}");
+                Console.WriteLine($"w={w} f={globalBestFitness}");
 
                 // Update velocity & position
-                for (int p = 0; p < npop; p++)
+                for (int p = 0; p < PopulationSize; p++)
                 {
-                    for (int m = 0; m < n; m++)
+                    for (int m = 0; m < numberOfVars; m++)
                     {
-                        v[p, m] = w * v[p, m] + c1 * random.NextDouble() * (ibest[p, m] - x[p][m])
-                                              + c2 * random.NextDouble() * (gbest[m] - x[p][m]);
-                        x[p][m] = x[p][m] + v[p, m];
+                        velocity[p, m] = w * velocity[p, m] + c1 * random.NextDouble() * (invididualBestPosition[p, m] - x[p][m])
+                                              + c2 * random.NextDouble() * (globalBestPosition[m] - x[p][m]);
+                        x[p][m] = x[p][m] + velocity[p, m];
                     }
                 }
                 // Intertia weight changes
