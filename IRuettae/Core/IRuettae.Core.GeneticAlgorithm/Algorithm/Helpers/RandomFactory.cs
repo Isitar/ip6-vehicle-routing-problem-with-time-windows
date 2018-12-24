ddc07b7c33dcbffccaceb8ec3aaa316f7ff2a798
Janik Meyer <janik.my@gmail.com>
2018-12-24 13:10:06 +0100

    made RandomFactory threadsafe

--- IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Helpers/RandomFactory.cs
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Helpers/RandomFactory.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IRuettae.Core.GeneticAlgorithm.Algorithm.Helpers
 {
+    /// <summary>
+    /// Threadsafe
+    /// </summary>
     internal sealed class RandomFactory
     {
-        /// <summary>
-        /// Warning: Random is not threadsafe
-        /// </summary>
-        public static Random Instance { get; } = new Random();
+        private static ThreadLocal<Random> instances = new ThreadLocal<Random>(() => new Random());
+
+        private RandomFactory()
+        {
+        }
+
+        public static Random Instance
+        {
+            get { return instances.Value; }
+        }
     }
 }
