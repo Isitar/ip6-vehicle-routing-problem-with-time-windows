25dbb61149502df99f3b21a676931c8ed37e34ad
Pascal Lüscher <luescherpascal@gmail.com>
2019-01-01 13:52:56 +0100

    target function factory gurobi

--- IRuettae/Core/IRuettae.Core.ILPIp5Gurobi/Algorithm/Scheduling/TargetFunctionBuilders/TargetFunctionFactory.cs
+++ IRuettae/Core/IRuettae.Core.ILPIp5Gurobi/Algorithm/Scheduling/TargetFunctionBuilders/TargetFunctionFactory.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Google.OrTools.LinearSolver;
-using IRuettae.Core.ILP.Algorithm.Scheduling.Detail;
+using Gurobi;
+using IRuettae.Core.ILPIp5Gurobi.Algorithm.Scheduling.Detail;
 
-namespace IRuettae.Core.ILP.Algorithm.Scheduling.TargetFunctionBuilders
+
+namespace IRuettae.Core.ILPIp5Gurobi.Algorithm.Scheduling.TargetFunctionBuilders
 {
     internal class TargetFunctionFactory
     {
@@ -23,7 +24,7 @@ namespace IRuettae.Core.ILP.Algorithm.Scheduling.TargetFunctionBuilders
         /// <param name="target"></param>
         /// <param name="weight">1 is minimal weight, use higher weight for more important targets</param>
         /// <returns>LinearExpr which should be minimised</returns>
-        public LinearExpr CreateTargetFunction(TargetType target, int weight = 1)
+        public GRBLinExpr CreateTargetFunction(TargetType target, int weight = 1)
         {
             switch (target)
             {
@@ -38,62 +39,59 @@ namespace IRuettae.Core.ILP.Algorithm.Scheduling.TargetFunctionBuilders
             }
         }
 
-        private LinearExpr CreateTargetFunctionTryVisitDesired(int weight)
+        private GRBLinExpr CreateTargetFunctionTryVisitDesired(int weight)
         {
-            var sum = new LinearExpr[solverData.NumberOfVisits];
-            sum[0] = new LinearExpr();
+            var sum = new GRBLinExpr(0);
+
             for (int visit = 1; visit < solverData.NumberOfVisits; visit++)
             {
-                sum[visit] = new LinearExpr();
                 for (int day = 0; day < solverData.NumberOfDays; day++)
                 {
                     for (int timeslice = 1; timeslice < solverData.SlicesPerDay[day]; timeslice++)
                     {
                         var isDesired = solverData.Input.Visits[day][visit, timeslice] == VisitState.Desired;
-                        sum[visit] -= solverData.Variables.Visits[day][visit, timeslice] * Convert.ToInt32(isDesired);
+                        sum -= solverData.Variables.Visits[day][visit][timeslice] * Convert.ToInt32(isDesired);
                     }
                 }
             }
 
-            return sum.Sum() * weight;
+            return sum * weight;
         }
 
-        private LinearExpr CreateTargetFunctionTryVisitEarly(int weight)
+        private GRBLinExpr CreateTargetFunctionTryVisitEarly(int weight)
         {
-            var maxWeight = 0.0;
-            var sum = new LinearExpr[solverData.NumberOfDays];
+            var sum = new GRBLinExpr(0);
+
             for (int day = 0; day < solverData.NumberOfDays; day++)
             {
-                sum[day] = new LinearExpr();
                 for (int timeslice = 1; timeslice < solverData.SlicesPerDay[day]; timeslice++)
                 {
                     for (int santa = 0; santa < solverData.NumberOfSantas; santa++)
                     {
-                        sum[day] += solverData.Variables.SantaEnRoute[day][santa, timeslice] * timeslice;
+                        sum += solverData.Variables.SantaEnRoute[day][santa][timeslice] * timeslice;
 
                     }
                 }
-                double slices = solverData.SlicesPerDay[day];
-
                 // gauss sum formula
-                maxWeight += (slices * slices + slices) / 2;
             }
 
-            return sum.Sum() * weight;
+            return sum * weight;
         }
 
-        private LinearExpr CreateTargetFunctionMinTime(int weight)
+        private GRBLinExpr CreateTargetFunctionMinTime(int weight)
         {
-            var sum = new LinearExpr[solverData.NumberOfDays];
+            var sum = new GRBLinExpr(0);
             for (int day = 0; day < solverData.NumberOfDays; day++)
             {
-                sum[day] = new LinearExpr();
-                foreach (var v in solverData.Variables.SantaEnRoute[day])
+                foreach (var sv in solverData.Variables.SantaEnRoute[day])
                 {
-                    sum[day] += v;
+                    foreach (var v in sv)
+                    {
+                        sum += v;
+                    }
                 }
             }
-            return sum.Sum() * weight;
+            return sum * weight;
         }
     }
 }
--- IRuettae/Core/IRuettae.Core.ILPIp5Gurobi/Algorithm/Scheduling/TargetFunctionBuilders/TargetType.cs
+++ IRuettae/Core/IRuettae.Core.ILPIp5Gurobi/Algorithm/Scheduling/TargetFunctionBuilders/TargetType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IRuettae.Core.ILP.Algorithm.Scheduling.TargetFunctionBuilders
+namespace IRuettae.Core.ILPIp5Gurobi.Algorithm.Scheduling.TargetFunctionBuilders
 {
     internal enum TargetType
     {
