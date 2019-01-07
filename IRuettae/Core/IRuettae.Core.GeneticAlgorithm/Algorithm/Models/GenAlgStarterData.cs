4a13b574f94c1263f8e719fb69a139a590349d6d
Janik Meyer <janik.my@gmail.com>
2019-01-07 11:06:06 +0100

    added bigger populationSizes to GA

--- IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Models/GenAlgStarterData.cs
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Models/GenAlgStarterData.cs
@@ -42,7 +42,7 @@ namespace IRuettae.Core.GeneticAlgorithm.Algorithm.Models
             // Debug Population size
             var populationSizes = new int[]
             {
-                2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,
+                2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,
             };
             starterData.PopulationSize = populationSizes[(callCounter++ / runs) % populationSizes.Length];
 #endif
