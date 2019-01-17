a7b643f421ad4b223a1a411a39267800b09dcaa4
Janik Meyer <janik.my@gmail.com>
2019-01-17 21:52:30 +0100

    draft of WayEvaluator in Google Routing

---
+++ IRuettae/Core/IRuettae.Core.Google.Routing/Algorithm/WayEvaluator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.OrTools.ConstraintSolver;
+using IRuettae.Core.Google.Routing.Models;
+
+namespace IRuettae.Core.Google.Routing.Algorithm
+{
+    public class WayEvaluator : NodeEvaluator2
+    {
+        private readonly RoutingData data;
+
+        public WayEvaluator(RoutingData data)
+        {
+            this.data = data ?? throw new ArgumentException("data must not be null");
+        }
+
+        public override long Run(int firstIndex, int secondIndex)
+        {
+            if (firstIndex >= data.Visits.Count || secondIndex >= data.Visits.Count)
+            {
+                throw new ArgumentOutOfRangeException("index must be smaller than numberOfVisits");
+            }
+
+            bool firstIsHome = firstIndex == data.HomeIndex || firstIndex == data.HomeIndexAdditional;
+            bool secondIsHome = secondIndex == data.HomeIndex || firstIndex == data.HomeIndexAdditional;
+
+            if (firstIsHome && secondIsHome)
+            {
+                // home to home is zero
+                return 0;
+            }
+            else if (firstIsHome)
+            {
+                return data.Visits[secondIndex].WayCostFromHome;
+            }
+            else if (secondIsHome)
+            {
+                return data.Visits[firstIndex].WayCostToHome;
+            }
+            else
+            {
+                // Todo
+            }
+            return 0;
+        }
+
+    }
+}
