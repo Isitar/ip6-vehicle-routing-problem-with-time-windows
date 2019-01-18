e11860f1a06a3fa9db1413c1924caa3217c4f714
Janik Meyer <janik.my@gmail.com>
2019-01-18 12:58:39 +0100

    added first steps to Google Routing solver

--- IRuettae/Core/IRuettae.Core.Google.Routing/RoutingSolver.cs
+++ IRuettae/Core/IRuettae.Core.Google.Routing/RoutingSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using IRuettae.Core.Google.Routing.Algorithm;
 using IRuettae.Core.Google.Routing.Models;
 using IRuettae.Core.Models;
 
@@ -17,7 +18,20 @@ namespace IRuettae.Core.Google.Routing
 
         public OptimizationResult Solve(long timeLimitMilliseconds, EventHandler<ProgressReport> progress, EventHandler<string> consoleProgress)
         {
-            CapacitatedVehicleRoutingProblemWithTimeWindows.Main(new String[0]);
+            //CapacitatedVehicleRoutingProblemWithTimeWindows.Main(new String[0]);
+
+            // transform input
+            var data = new RoutingData(input);
+            new SantaCreator(data).Create(starterData.MaxNumberOfSantas);
+            new VisitCreator(data).Create();
+            new UnavailableCreator(data).Create();
+
+            // create RoutingModel
+            //new RoutingModelCreator(data).Create();
+
+            // create solver settings
+
+
             throw new NotImplementedException();
         }
     }
