8c9641d70764209b294e8059846f032a6a006f40
Janik Meyer <janik.my@gmail.com>
2019-01-23 12:28:24 +0100

    added unit test for Google Routing solver

---
+++ IRuettae/Core/IRuettae.Core.Google.Routing.Tests/RoutingSolverTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IRuettae.Core.Google.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IRuettae.Core.Google.Routing.Tests.Algorithm;
+using IRuettae.Core.Google.Routing.Models;
+
+namespace IRuettae.Core.Google.Routing.Tests
+{
+    [TestClass()]
+    public class RoutingSolverTests
+    {
+        [TestMethod()]
+        public void TestSolve_Simple()
+        {
+            var input = Testdataset1.Create();
+            var solver = new RoutingSolver(input, new RoutingSolverStarterData
+            {
+                MaxNumberOfSantas = 2,
+            });
+
+            var actual = solver.Solve(10, null, null);
+
+            Assert.IsTrue(actual.IsValid(), actual.Validate());
+        }
+
+        [TestMethod()]
+        public void TestSolve_AdditionalSanta()
+        {
+            var input = Testdataset1.Create();
+            var solver = new RoutingSolver(input, new RoutingSolverStarterData
+            {
+                MaxNumberOfSantas = 3,
+            });
+
+            var actual = solver.Solve(10, null, null);
+
+            Assert.IsTrue(actual.IsValid(), actual.Validate());
+        }
+    }
+}
