b4adfe1cc40cb6631d02b6407f80be0d4b7700fe
Pascal Lüscher <luescherpascal@gmail.com>
2019-03-04 15:38:14 +0100

    added GA reruns of dataset 9 - 11

---
+++ Results/evaluation/GA RESULTS/9-11Rerun2/run.bat
@@ -0,0 +1,19 @@
+..\..\IRuettae.Evaluator.exe 2 9 1
+..\..\IRuettae.Evaluator.exe 2 10 1
+..\..\IRuettae.Evaluator.exe 2 11 1
+
+..\..\IRuettae.Evaluator.exe 2 9 1
+..\..\IRuettae.Evaluator.exe 2 10 1
+..\..\IRuettae.Evaluator.exe 2 11 1
+
+..\..\IRuettae.Evaluator.exe 2 9 1
+..\..\IRuettae.Evaluator.exe 2 10 1
+..\..\IRuettae.Evaluator.exe 2 11 1
+
+..\..\IRuettae.Evaluator.exe 2 9 1
+..\..\IRuettae.Evaluator.exe 2 10 1
+..\..\IRuettae.Evaluator.exe 2 11 1
+
+..\..\IRuettae.Evaluator.exe 2 9 1
+..\..\IRuettae.Evaluator.exe 2 10 1
+..\..\IRuettae.Evaluator.exe 2 11 1
