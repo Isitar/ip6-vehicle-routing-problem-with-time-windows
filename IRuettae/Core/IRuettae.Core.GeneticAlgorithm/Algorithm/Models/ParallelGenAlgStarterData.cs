1a7d4bda8076d509e0716417aa942a8824147393
Janik Meyer <janik.my@gmail.com>
2019-03-08 15:53:07 +0100

    implemented Parallel GA for convinience

---
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/Algorithm/Models/ParallelGenAlgStarterData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using IRuettae.Core.Models;
+
+namespace IRuettae.Core.GeneticAlgorithm.Algorithm.Models
+{
+    public class ParallelGenAlgStarterData : IStarterData
+    {
+        public GenAlgStarterData GenAlgStarterData { get; set; }
+        public int NumberOfRuns { get; set; }
+
+        public ParallelGenAlgStarterData(GenAlgStarterData starterData, int numberOfRuns)
+        {
+            GenAlgStarterData = starterData;
+            NumberOfRuns = numberOfRuns;
+        }
+
+        public override string ToString() => string.Join(Environment.NewLine, GetType().GetProperties().Select(p => $"{p.Name}: {(p.GetIndexParameters().Length > 0 ? "Indexed Property cannot be used" : p.GetValue(this, null))}"));
+    }
+}
---
+++ IRuettae/Core/IRuettae.Core.GeneticAlgorithm/ParallelGenAlgSolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IRuettae.Core.GeneticAlgorithm.Algorithm.Models;
+using IRuettae.Core.Models;
+
+namespace IRuettae.Core.GeneticAlgorithm
+{
+    /// <summary>
+    /// Implementation that starts one genetic algorithm per core.
+    /// </summary>
+    public class ParallelGenAlgSolver : ISolver
+    {
+        private readonly OptimizationInput input;
+        private readonly GenAlgStarterData starterData;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="starterData"></param>
+        public ParallelGenAlgSolver(OptimizationInput input, GenAlgStarterData starterData)
+        {
+            if (!starterData.IsValid())
+            {
+                throw new ArgumentException("The given GenAlgStarterData is invalid.");
+            }
+
+            this.input = input;
+            this.starterData = starterData;
+        }
+
+        public OptimizationResult Solve(long timeLimitMilliseconds, EventHandler<ProgressReport> progress, EventHandler<string> consoleProgress)
+        {
+            consoleProgress?.Invoke(this, "Solving started");
+            progress?.Invoke(this, new ProgressReport(0.01));
+
+            var orderedResults = Enumerable.Range(0, Environment.ProcessorCount)
+                .AsParallel()
+                .Select(v =>
+                {
+                    var log = new StringBuilder();
+                    var result = new GenAlgSolver(input, starterData).Solve(timeLimitMilliseconds, null, (obj, msg) => log.AppendLine(msg));
+                    return (result: result, log: log.ToString());
+                })
+                .OrderBy(run => run.result.Cost())
+                .ToArray();
+            var bestResult = orderedResults.First();
+
+            consoleProgress?.Invoke(this, bestResult.log);
+
+            consoleProgress?.Invoke(this, "Internal solving finished.");
+            for (int i = 1; i < orderedResults.Length; i++)
+            {
+                consoleProgress?.Invoke(this, $"Run {i + 1} costs {orderedResults[i].result.Cost()}.");
+            }
+            progress?.Invoke(this, new ProgressReport(0.99));
+
+            return bestResult.result;
+        }
+    }
+}
